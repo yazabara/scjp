2a2801d262c033b3762ecd0ff9c7af298745a326
yazabara <17513901Github>
2015-03-05 21:15:51 +0300

    book part 1 - classes access

--- src/scjp6/book/part1/bar/Bar.java
+++ src/scjp6/book/part1/bar/Bar.java
@@ -5,11 +5,28 @@ import scjp6.book.part1.foo.Foo;
 /**
  * Created by Yaroslav on 05.03.2015.
  */
-public class Bar implements Foo{
+public class Bar extends Roo implements Foo{
 
     @Override
     public void go() {
-        //cannot change final static variable - becouse interface
+        //cannot change final static variable - because interface
         //variable = 10;
+    }
+
+    public Bar() {
+        //private
+        //doSomeAction();
+        doSomeAction("Yeeee");//OK
+    }
+
+    public void doSomeAction() {
+        //another method for Bar class
+        System.out.println("Bar doSomeAction method");
+    }
+
+    /** Overriding method */
+    @Override
+    public void doSomeAction(String a) {
+        System.out.println("Bar doSomeAction method with params");
     }
 }
---
+++ src/scjp6/book/part1/bar/Roo.java
@@ -0,0 +1,15 @@
+package scjp6.book.part1.bar;
+
+/**
+ * Created by Yaroslav on 05.03.2015.
+ */
+public class Roo {
+
+    private void doSomeAction() {
+        System.out.println("private method");
+    }
+
+    public void doSomeAction(String a) {
+        System.out.println("public method");
+    }
+}
